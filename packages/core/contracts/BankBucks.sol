b4b13cf3a7f9f7d749db403deb4d54b3a4f52614
Michael Mueller <hello@michaelmueller.dev>
2021-02-09 16:23:08 +0100

    fix merge error

--- packages/core/contracts/BankBucks.sol
+++ packages/core/contracts/BankBucks.sol
@@ -3,15 +3,8 @@ pragma solidity >=0.6.0 <0.7.0;
 import 'hardhat/console.sol';
 import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
 
-<<<<<<< HEAD:packages/core/contracts/BankBucks.sol
 contract BankBucks is ERC20 {
     constructor() public ERC20('BankBucks', 'BKB') {
         _mint(msg.sender, 40000 * 10**18);
-=======
-contract BankBucks is ERC20{
-    constructor() ERC20("BankBucks", "BKB") public {
-        // _mint(to, 40000 * 10 **18);
-        _mint(msg.sender, 40000 * 10 **18);
->>>>>>> main:packages/core/contracts/test/BankBucks.sol
     }
 }
