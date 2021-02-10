99d54d8eb92321ee3bd34d5cb0dbfea0cac11ccd
Michael Mueller <hello@michaelmueller.dev>
2021-02-10 01:14:22 +0100

    override AntD styles

--- packages/interface/craco.config.js
+++ packages/interface/craco.config.js
@@ -1,35 +1,15 @@
 const CracoAntDesignPlugin = require('craco-antd');
-const CracoLessPlugin = require('craco-less');
 
 module.exports = {
   plugins: [
-    {
-      plugin: CracoLessPlugin,
-      options: {
-        lessLoaderOptions: {
-          // noIeCompat: true,
-          lessOptions: {
-            modifyVars: {
-              '@primary-color': '#1DA57A',
-              '@link-color': '#1DA57A',
-              '@border-radius-base': '2px',
-              '@text-color': '#faad14',
-              '@font-size-base': '40px',
-            },
-            javascriptEnabled: true,
-          },
-        },
-      },
-    },
     {
       plugin: CracoAntDesignPlugin,
       options: {
         customizeTheme: {
           '@primary-color': '#1DA57A',
           '@link-color': '#1DA57A',
-          '@border-radius-base': '2px',
-          '@text-color': '#faad14',
-          '@font-size-base': '40px',
+          '@border-radius-base': '15px',
+          '@font-size-base': '16px',
         },
       },
     },
