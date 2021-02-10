4b69a3c40d4b527698e1817a3cc2e3308f2c43a4
Michael Mueller <hello@michaelmueller.dev>
2021-02-10 15:15:31 +0100

    bot can reply to a tweet

--- packages/server/bot.js
+++ packages/server/bot.js
@@ -2,20 +2,56 @@
 require('dotenv').config();
 const Twit = require('twit');
 
-const T = new Twit({
+const bot = new Twit({
   consumer_key: process.env.API_KEY,
   consumer_secret: process.env.API_SECRET_KEY,
   access_token: process.env.ACCESS_TOKEN,
   access_token_secret: process.env.ACCESS_TOKEN_SECRET,
 });
 
-// start stream and track tweets
-// const stream = T.stream('statuses/filter', { track: 'wager' });
+// const stream = bot.stream('statuses/filter', { track: 'wager' });
+const stream = bot.stream('statuses/filter', { track: '@michaelbcn' });
+
+function tweetEvent(tweet) {
+  console.log('tweetEvent triggered', tweet);
+
+  const {
+    id,
+    text,
+    user: { screen_name },
+  } = tweet;
+  const reply = `@${screen_name} check out https://miniguide.co`;
+
+  console.log('id', id);
+  console.log('tweetText', text);
+  console.log('replyTo', screen_name);
+  console.log('reply', reply);
+
+  const callback = (err, data) => {
+    if (err) console.log(err.mesage);
+    else console.log(`Tweeted: ${data.text}`);
+  };
+  bot.post('statuses/update', { status: reply, in_reply_to_status_id: id }, callback);
+}
+
+const startBot = async () => {
+  console.log('bot listening to stream');
+  stream.on('tweet', tweetEvent);
+};
+
+const stopBot = async () => {
+  console.log('bot stopped listening to stream');
+  stream.stop();
+};
+
+// const stream = bot.stream('statuses/filter', { track: '@michaelbcn' });
+
+// stream.on('tweet', tweetEvent);
 
 // use this to log errors from requests
-function responseCallback(err, data, response) {
-  console.log(err);
-}
+// function responseCallback(err, data, response) {
+//   console.log(err);
+// }
 
 // event handler
 // stream.on('tweet', tweet => {
@@ -23,11 +59,13 @@ function responseCallback(err, data, response) {
 //   console.log('\n\n------------------------------------------\n\n');
 //   console.log(`${created_at} ${user.name} ${text}`);
 // retweet
-// T.post('statuses/retweet/:id', { id: tweet.id_str }, responseCallback);
+// bot.post('statuses/retweet/:id', { id: tweebot.id_str }, responseCallback);
 // like
-// T.post('favorites/create', { id: tweet.id_str }, responseCallback);
+// bot.post('favorites/create', { id: tweebot.id_str }, responseCallback);
 // });
 
-T.post('statuses/update', { status: 'hello world!' }, function (err, data, response) {
-  console.log(data);
-});
+// bot.post('statuses/update', { status: 'hello world!' }, function (err, data, response) {
+//   console.log(data);
+// });
+
+module.exports = { startBot, stopBot };
--- packages/server/routes/index.js
+++ packages/server/routes/index.js
@@ -2,8 +2,20 @@ const express = require('express');
 
 const router = express.Router();
 
+const { startBot, stopBot } = require('../bot');
+
 router.get('/hello', (req, res) => {
   res.status(202).json({ code: 'hello' });
+});
+
+router.get('/start-bot', async (req, res) => {
+  await startBot();
+  res.status(202).json({ code: 'bot-started' });
+});
+
+router.get('/stop-bot', async (req, res) => {
+  await stopBot();
+  res.status(202).json({ code: 'bot-stopped' });
 });
 
 module.exports = router;
