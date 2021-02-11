31e27c27ffc88a13239aa82b1f0e189b58bc3a4d
Michael Mueller <hello@michaelmueller.dev>
2021-02-11 12:08:03 +0100

    ui-flow

--- packages/interface/src/hooks/Collateral.js
+++ packages/interface/src/hooks/Collateral.js
@@ -1,15 +1,23 @@
 import { useState, useEffect } from 'react';
+import usePoller from './Poller';
 
 export default function useWager(wager, wagerAddress) {
+  let pollTime = 1777;
+
   const [collateral, setCollateral] = useState();
-  useEffect(() => {
-    if (!wager || wagerAddress === '0x0000000000000000000000000000000000000000') return;
-    const getCollateral = async () => {
-      const result = await wager.getCollateral();
-      setCollateral(result);
-    };
-    getCollateral();
-  }, [wager, wagerAddress]);
+
+  usePoller(
+    async () => {
+      if (!wager || wagerAddress === '0x0000000000000000000000000000000000000000') return;
+      const getCollateral = async () => {
+        const result = await wager.getCollateral();
+        setCollateral(result);
+      };
+      getCollateral();
+    },
+    pollTime,
+    wager && wagerAddress,
+  );
 
   return collateral;
 }
